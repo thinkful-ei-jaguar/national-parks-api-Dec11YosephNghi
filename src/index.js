a5724db4152b24d267d4428d43379d90e7679994
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-11 14:23:23 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import $ from 'jquery';
 
+function htmlTemplate() {
+  return `<li> 
+ <p>Full name</p>
+ <p>Description</p>
+ <p>Website URL</p>
+ </li>
+ `;
+}
+
+function searchParks() {
+  // Listens to when user clicks submit
+  $('.js-form').submit(function (event) {
+    event.preventDefault();
+    let states = $('js-states').val();
+    let statessearch = states.split(' ');
+    console.log(statessearch);
+    let numResults = $('js-numOfResult').val();
+  });
+}
+
 function main() {
   console.log('DOM is loaded');
-
+  searchParks();
   const startMsg = $('<p>Webpack is working!</p>');
   $('#root').append(startMsg);
 }
