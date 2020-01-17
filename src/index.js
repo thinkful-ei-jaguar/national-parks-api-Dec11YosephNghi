3a4b994b38fc9015f6ba5a65b3b6a445761f67af
Nghi Tran <nghi.t.tran8@gmail.com>
2020-01-16 16:34:58 -0800

    update api feature to display results

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 import $ from 'jquery';
 
-function htmlTemplate() {
+function htmlTemplate(name, des, url) {
   return `<li> 
- <p>Full name</p>
- <p>Description</p>
- <p>Website URL</p>
+ <p>${name}</p>
+ <p>${des}</p>
+ <p>${url}</p>
  </li>
  `;
+}
+
+function printList(data) {
+  $('.results').html(data.map(park =>
+    htmlTemplate(park.fullName, park.description, park.url)));
+}
+
+function callAPI(states, num) {
+  const stateQuery = 'stateCode=' + states;
+  const apiKey = 'api_key=6QEP3kxADqkQeVG9008RiZTaidHle4iLaSSvWJmI';
+  const limit = 'limit=' + num;
+  fetch(`https://developer.nps.gov/api/v1/parks?${stateQuery}&${limit}&${apiKey}`)
+    .then(res => res.ok ? res.json() : Promise.reject('Cannot fetch parks data'))
+    .then(res => printList(res.data));
 }
 
 function searchParks() {
   // Listens to when user clicks submit
   $('.js-form').submit(function (event) {
     event.preventDefault();
-    let states = $('js-states').val();
-    let statessearch = states.split(' ');
-    console.log(statessearch);
-    let numResults = $('js-numOfResult').val();
+    let states = $('.js-states').val();
+    let numResults = $('.js-numOfResult').val();
+    callAPI(states, numResults.toString());
   });
 }
 
 function main() {
-  console.log('DOM is loaded');
   searchParks();
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
 }
 
 $(main);
